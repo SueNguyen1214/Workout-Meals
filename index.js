daad9f986c37800df401a8d745916b7a3e70170b
Sue Nguyen <SueNguyen1214@gmail.com>
2020-10-13 21:54:24 -0700

    fix the max return value for the first option

--- index.js
+++ index.js
@@ -2,7 +2,41 @@
 const youtubeApiKey="AIzaSyDkYi5rExId9p1BkHm08Iav3Wz_aotGYsI";
 const spoonApiKey="";
 const youtubeURL="https://www.googleapis.com/youtube/v3/search";
-//const STORE={} for key-value paif of type of esercise and calories burned
+//for key-value paif of type of esercise and calories burned
+const MET={
+    aerobics: 7.3,
+    "water aerobics": 5.3,
+    badminton: 5.5,
+    "competitive badminton": 7.0,
+    ballet: 5,
+    basketball: 6.5,
+    "competitive basketball": 8.0,
+    boxing: 12.8,
+    dancing:7.8,
+    goft: 4.8,
+    gymnastics: 3.8,
+    hiking:7.8,
+    "horseback riding": 5.5,
+    jogging:7.0,
+    kickboxing: 10.3,
+    pilates:3.0,
+    racquetball: 7.0,
+    "competitive racquetball": 10.0,
+    "rope jumping": 11.8,
+    "slow run":6.0,
+    "moderate run": 11.0,
+    "fast run":16.0,
+    squats: 5.0,
+    "tai chi": 3.0,
+    tennis: 7.3,    
+    swimming: 4.0,
+    walking:9.5,
+    "weight lifting": 6.0,
+    zumba:6.5,
+    Xbox: 6.3,
+    "Hatha yoga": 2.5,
+    "Power yoga": 4.0,
+} 
 
 //this function is to format the Query params for Youtube
 function formatYoutubeQueryParams(params){
@@ -10,20 +44,14 @@ function formatYoutubeQueryParams(params){
     return queryItem.join("&");
 };
 //this function is to display the Youtube videos
-function displayYoutube(responseJson, maxReturn){
+function displayYoutube(responseJson, maxResults){
     $('.currentDisplay').html("");//to clear out the previous results
-    for (let i = 0; i < maxReturn; i++) {
+     $('.currentDisplay').append(`
+     <h4>Here are your exercise video.</h4>`);
+    for (let i = 0; i < maxResults; i++) {
         console.log(responseJson.items[i].id.videoId);
         const link = `https://www.youtube.com/watch?v=${responseJson.items[i].id.videoId}`;
         $('.currentDisplay').append(`
-        <h3>Here are your exercise video</h3>
-        <p>Would you like to know how many calories you migh be burning?</p>
-        <form>
-            <input id = "calYes" type = "radio" name = "yesToCal" value = "yes">
-            <label for = "calYes"> Yes, show me </label> <br>
-            <input id = "calNo" type = "radio" name = "noToCal" value = "no">
-            <label for = "calNo"> No, I'm good </label>
-        </form>
         <h4>Title: ${responseJson.items[i].snippet.title}</h4>
         <p>Description: ${responseJson.items[i].snippet.description}</p>
         <a href='${link}' target='_blank'>
@@ -35,14 +63,14 @@ function displayYoutube(responseJson, maxReturn){
 };
 
 //This function is to fetch the videos from Youtube
-function getYoutubeVideo(chosenExercise,maxReturn){
+function getYoutubeVideo(chosenExercise,maxResults = 10){
     const params = {
         key:youtubeApiKey, 
         q:chosenExercise,
         part:'snippet',
-        maxReturn,
+        maxResults,
         type:'video'             
-    }
+    };
     const paramQuery = formatYoutubeQueryParams(params);
     console.log(paramQuery);
     const url =  youtubeURL + "?" + paramQuery;
@@ -53,7 +81,7 @@ function getYoutubeVideo(chosenExercise,maxReturn){
                 return response.json();
             } throw new Error (response.statusText);
         })
-        .then (responseJson => displayYoutube(responseJson,maxReturn))
+        .then (responseJson => displayYoutube(responseJson,maxResults))
         .catch(err => {
             $('.currentDisplay').text(`Something went wrong. ${err.message}`);
         })
@@ -62,16 +90,23 @@ function getYoutubeVideo(chosenExercise,maxReturn){
 //This function is to display the calorie screen
 function calorieScreen(){
     $('.currentDisplay').html(`
-    <p>What type of exercise/activities have you done today?</p>
-    <p>What is the duration of the activities?</p>
-    <p>What is your current weight? </p>`)
+    <form id = "calorieCal">
+        <label for = "exerciseDone">What type of exercise/activities have you done today?</label>
+        <input id = "js-exerciseDone" type = "text" placeholder = "Hatha Yoga, Power Yoga, basketball,competitive basketball"><br>
+        <label for = "duration">How many hours did you exercise (partial hours please enter in quarter ( 0.25) increments?</label>
+        <input id ="js-duration" type = "number" value = "1.0" step = "0.25" min = "0" ><br>
+        <label for = "weight">What is your current weight in pounds? </label>
+        <input id = "js-weight" type = "number" value = "120.0" min = "5"><br>
+        <button type = "submit" id = "submitCalorie">Calculate</button>
+    </form>`)
+    watchCalculate();
 }
-//This function is to watch for the answer to the Exercise question
-function watchExerciseOption(){
-    $('#yes').on('click',(event) =>{
+//This function is to watch for which option the users choose from the first screen
+function watchOption(){
+    $('#exerciseOnly').on('click',(event) =>{
         event.preventDefault();
         $('.currentDisplay').html(`
-        <form id = 'workoutType'>
+        <form id = 'excerciseChoice'>
             <label for = "exerciseType" >Alrighty. What kind of exercise do you have in mind ? </label>
             <input id = "js-exerciseType" type = "text" placeholder = "Yoga, kickboxing" name="exerciseType">
             <br>
@@ -82,7 +117,7 @@ function watchExerciseOption(){
         )
         watchSearchVideo();
     });
-    $('#noWithCal').on('click', (event) =>{
+    $('#calorieOnly').on('click', (event) =>{
         event.preventDefault();
         calorieScreen();
     });
@@ -105,21 +140,45 @@ function watchExerciseOption(){
     })
     
 }
-//This fuction is to watch for the Seacrh Video Button
+//This fuction is to watch for the Search Video button
 function watchSearchVideo(){
     $('form').submit(event => {
         event.preventDefault();
         const chosenExercise = $('#js-exerciseType').val();
         console.log(chosenExercise);
-        const maxReturn=$('#js-maxResult').val();
-        console.log(maxReturn);
-        getYoutubeVideo(chosenExercise, maxReturn);
+        const maxResults=$('#js-maxResult').val();
+        console.log(maxResults);
+        getYoutubeVideo(chosenExercise, maxResults);
     });
 };
-
+//This function is to watch for the Calculate button
+function watchCalculate(){
+    $('#submitCalorie').submit(event => {
+        event.preventDefault();
+        const exerciseDoneType = $('#js-exerciseDone').val();
+        const durationDone = $('#js-duration').val();
+        const weightDone = $('#js-weight').val();
+    })
+    caloriesCalculation(exerciseDoneType, durationDone, weightDone);
+};
 
 //This function is for the calories counting part
-function caloriesCalculation(){
+function caloriesCalculation(exerciseDoneType, durationDone, weightDone){
+    var result =0;
+    var weightInKg=weightDone*0.453592;
+    console.log(weightInKg);
+    for ( key in MET){
+        if (exerciseDoneType === key){
+            result += weightInKg*durationDone*MET.key;
+        }
+    }
+    if (result === 0){
+        $('.currentDisplay').html(`
+        <p>I'm sorry. We don't have info for this type of exercise</p>`)
+    }else{
+        $('.currentDisplay').html(`
+        <p>Here is how much calories you have burned: ${result}</p>`)
+    };
 
 }
 //This function is to form the query params for Spponacular
@@ -140,6 +199,6 @@ function watchSearchRecipe(){
 }
 
 function renderApp(){
-    watchExerciseOption();
+    watchOption();
 }
 $(renderApp);
